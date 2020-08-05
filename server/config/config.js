55244661346afc6b1fd9a0e9940b15214699a7ba
Rogelio Pacheco <rogelio.pacheco@desclub.com.mx>
2020-08-04 19:32:46 -0500

    Add Environment Variable to MongoDB URL

--- server/config/config.js
+++ server/config/config.js
@@ -5,6 +5,6 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev' ;
 
 //DB URL
-let DBurl = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : 'mongodb+srv://pchko:PAcheko666@cluster0.iprfl.mongodb.net/cafe';
+let DBurl = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : process.env.MONGO_URI;
 
 process.env.URLDB = DBurl;
